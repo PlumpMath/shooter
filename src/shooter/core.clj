a15756d4619b5291ebc05dd3142ad195b30bbfb6
Erik <erik.svedang@gmail.com>
2013-08-12 00:18:38 +0200

    Everything works.

--- src/shooter/core.clj
+++ src/shooter/core.clj
@@ -11,7 +11,6 @@
 (defn setup [world-atom]
   (frame-rate 30)
   (swap! world-atom world/add-state-fn states/maybe-more-enemies)
-  (swap! world-atom world/clear-ents)
   (swap! world-atom world/add-entity (player/create-player 50 50)))
 
 (defn update-and-draw [world-atom]
@@ -26,7 +25,8 @@
                  :key-pressed #(keyboard/key-pressed world-atom)
                  :key-released #(keyboard/key-released world-atom)
                  :size [(:width @world-atom) (:height @world-atom)])]
-    (swap! world-atom assoc :sketch sketch)))
+    (swap! world-atom assoc :sketch sketch))
+  world-atom)
 
 (defn -main [& args]
   (-> (atom (world/create-world 1024 768))
